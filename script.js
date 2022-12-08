1eca23a776c933697ba4647f3049895b2105b207
Vishal Singh <115806971+vishalkrsinghh@users.noreply.github.com>
2022-12-08 23:39:57 +0530

    1st

---
+++ script.js
@@ -0,0 +1,21 @@
+let innerDiv = document.getElementById('inner');
+let clock = () => {
+    let date = new Date();
+    let hour = date.getHours();
+    let pm;
+    let months = ['jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    if (hour > 12) {
+        hour = hour - 12;
+        pm = "PM";
+    }
+    else {
+        pm = "AM";
+    }
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    innerDiv.innerHTML = `<h3>${hour}:${minutes}:${seconds} ${pm}
+   &nbsp ${days[date.getDay()]} , ${months[date.getMonth()]}</h3>`;
+
+}
+setInterval(clock, 1000);
